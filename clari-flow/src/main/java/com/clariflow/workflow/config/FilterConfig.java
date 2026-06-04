c767c6e8630ad16e57202ec8f91d9a73a9a51e0d
dingyuanjing <kalen.ding@cninsights.com>
2026-06-04 09:50:09 +0800

    fix: JwtAuthFilter 改为 @Bean 注入，修复启动失败 (NoSuchBeanDefinition)

--- clari-flow/src/main/java/com/clariflow/workflow/config/FilterConfig.java
+++ clari-flow/src/main/java/com/clariflow/workflow/config/FilterConfig.java
@@ -1,6 +1,7 @@
 package com.clariflow.workflow.config;
 
 import com.clariflow.workflow.common.JwtAuthFilter;
+import com.clariflow.workflow.common.JwtUtil;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,11 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("!test")
 public class FilterConfig {
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter(JwtUtil jwtUtil) {
+        return new JwtAuthFilter(jwtUtil);
+    }
 
     @Bean
     public FilterRegistrationBean<JwtAuthFilter> jwtFilter(JwtAuthFilter filter) {
